737a78b4a4158ed352d3fe16aff73eca4b0cbde0
Felix Vogele <felix.vogele@bwz-rappi.ch>
2025-11-19 08:32:21 +0100

    fix login and register

--- backend/src/proxy.ts
+++ backend/src/proxy.ts
@@ -2,6 +2,9 @@ import { NextResponse } from 'next/server'
 
 export async function proxy() {
   const res = NextResponse.next();
+  res.headers.append('access-control-allow-origin', '*');
+  res.headers.append('access-control-allow-method', '*');
+  res.headers.append('access-control-allow-headers', '*');
   return res;
 }
 
